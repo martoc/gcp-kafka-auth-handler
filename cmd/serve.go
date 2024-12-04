0ed226bd1446f1aeb096a4c7ecd35047392148ea
martoc <octaviomartin@gmail.com>
2024-12-04 18:33:00 +0000

    fix: Change default port for API server from 8080 to 14293

--- cmd/serve.go
+++ cmd/serve.go
@@ -7,15 +7,13 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const (
-	defaultPort = 8080
-)
+const defaultPort = 14293
 
 var serveCmd = &cobra.Command{
 	Use:   "serve",
 	Short: "Serve the API",
 	Long:  `Start the API server`,
-	Run: func(_ *cobra.Command, _ []string) {
+	Run: func(cmd *cobra.Command, _ []string) {
 		log.Default().Printf("Version: v%s", CLIVersion)
 		handler.StartServer(defaultPort)
 	},
